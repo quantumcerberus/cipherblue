deb18247961d1aa772d4cca6e0a761441c228683
quantumcerberus <182113870+quantumcerberus@users.noreply.github.com>
2024-11-29 20:40:36 +0530

    Update file-permission-hardening.sh

--- files/scripts/desktop/file-permission-hardening.sh
+++ files/scripts/desktop/file-permission-hardening.sh
@@ -14,3 +14,5 @@ sed -i 's/^UMASK.*/UMASK 077/g' /etc/login.defs
 sed -i 's/^HOME_MODE/#HOME_MODE/g' /etc/login.defs
 sed -i 's/umask 022/umask 077/g' /etc/bashrc
 sed -i 's/\s+nullok//g' /etc/pam.d/system-auth
+
+sed -i 's@DefaultZone=FedoraWorkstation@DefaultZone=drop@g' /etc/firewalld/firewalld.conf
