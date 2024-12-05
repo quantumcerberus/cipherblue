aa165ba170333237068641211788d45caf8d730b
quantumcerberus <182113870+quantumcerberus@users.noreply.github.com>
2024-12-05 08:21:07 +0530

    Add files via upload

--- files/scripts/selinux/flatpakfull/flatpakfull.sh
+++ files/scripts/selinux/flatpakfull/flatpakfull.sh
@@ -1,1 +1,12 @@
+echo "Building and Loading Policy"
 
+set -x
+
+
+make -f /usr/share/selinux/devel/Makefile flatpakfull.pp || exit
+/usr/sbin/semodule -i flatpakfull.pp
+
+
+/sbin/restorecon -F -R -v /usr/bin
+
+/sbin/restorecon -F -R -v /usr/libexec/
