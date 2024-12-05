5f9580cb5ff7150322cf5fbdb03a42547c2e0944
quantumcerberus <182113870+quantumcerberus@users.noreply.github.com>
2024-12-05 08:22:42 +0530

    Create hardenuserns.sh

---
+++ files/scripts/hardenuserns.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -oue pipefail
+
+rpm-ostree install selinux-policy-devel
+
+cd ./selinux/chromium
+bash chromium.sh
+cd ../..
+
+cd ./selinux/flatpakfull
+bash flatpakfull.sh
+cd ../..
+
+semodule -i ./selinux/user_namespace/grant_userns.cil
+semodule -i ./selinux/user_namespace/harden_userns.cil
