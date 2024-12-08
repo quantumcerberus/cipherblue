a0cb01ff3231475e80622ccfe4437cec282c63de
quantumcerberus <182113870+quantumcerberus@users.noreply.github.com>
2024-12-08 15:22:45 +0530

    Add files via upload

---
+++ files/scripts/selinux/nautilus/nautilus.sh
@@ -0,0 +1,8 @@
+echo "Building and Loading Policy"
+
+set -x
+
+make -f /usr/share/selinux/devel/Makefile nautilus.pp || exit
+/usr/sbin/semodule -i nautilus.pp
+
+/sbin/restorecon -F -R -v /usr/bin/nautilus
