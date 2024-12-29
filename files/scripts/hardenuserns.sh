c603cf6e6fcb4021e58cd58b400fe54dcb090c9f
quantumcerberus <182113870+quantumcerberus@users.noreply.github.com>
2024-12-29 18:26:03 +0530

    Update hardenuserns.sh

--- files/scripts/hardenuserns.sh
+++ files/scripts/hardenuserns.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
+# Tell build process to exit if there are any errors.
 set -oue pipefail
 
-dnf5 install --setopt=install_weak_deps=False selinux-policy-devel
+rpm-ostree install selinux-policy-devel
 
 cd ./selinux/chromium
 bash chromium.sh
@@ -19,3 +20,4 @@ cd ../..
 semodule -i ./selinux/user_namespace/grant_userns.cil
 semodule -i ./selinux/user_namespace/harden_userns.cil
 semodule -i ./selinux/user_namespace/harden_container_userns.cil
+semodule -i ./selinux/flatpakfull/grant_systemd_flatpak_exec.cil
