e8c3d19ae51aa74114bc255bd922e48338c72b4f
quantumcerberus <182113870+quantumcerberus@users.noreply.github.com>
2025-02-01 11:25:31 +0530

    Update cipherblue-signing.sh

--- modules/cipherblue-signing/cipherblue-signing.sh
+++ modules/cipherblue-signing/cipherblue-signing.sh
@@ -11,6 +11,30 @@ IMAGE_REGISTRY_TITLE=$(echo "$IMAGE_REGISTRY" | cut -d'/' -f2-)
 echo "Setting up container signing in cosign.yaml for $IMAGE_NAME"
 echo "Registry to write: $IMAGE_REGISTRY"
 
+if ! [ -d "$CONTAINER_DIR" ]; then
+    mkdir -p "$CONTAINER_DIR"
+fi
+
+if ! [ -d "$ETC_CONTAINER_DIR" ]; then
+    mkdir -p "$ETC_CONTAINER_DIR"
+fi
+
+if ! [ -d $CONTAINER_DIR/registries.d ]; then
+   mkdir -p "$CONTAINER_DIR/registries.d"
+fi
+
+if ! [ -d $ETC_CONTAINER_DIR/registries.d ]; then
+   mkdir -p "$ETC_CONTAINER_DIR/registries.d"
+fi
+
+if ! [ -d "/usr/etc/pki/containers" ]; then
+    mkdir -p "/usr/etc/pki/containers"
+fi
+
+if ! [ -d "/etc/pki/containers" ]; then
+    mkdir -p "/etc/pki/containers"
+fi
+
 cp "/etc/pki/containers/$IMAGE_NAME.pub" "/usr/etc/pki/containers/$IMAGE_REGISTRY_TITLE.pub"
 cp "/etc/pki/containers/$IMAGE_NAME.pub" "/etc/pki/containers/$IMAGE_REGISTRY_TITLE.pub"
 rm "/etc/pki/containers/$IMAGE_NAME.pub"
