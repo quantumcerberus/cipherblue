030820d3a2dfb440de37f3d783a1148ea737f792
quantumcerberus <182113870+quantumcerberus@users.noreply.github.com>
2025-02-03 11:18:37 +0530

    Update trivalent.sh

--- files/scripts/selinux/trivalent/trivalent.sh
+++ files/scripts/selinux/trivalent/trivalent.sh
@@ -4,8 +4,7 @@ echo "Building and Loading Policy"
 
 set -x
 
-make -f /usr/share/selinux/devel/Makefile chromium.pp || exit
-/usr/sbin/semodule -i chromium.pp
+make -f /usr/share/selinux/devel/Makefile trivalent.pp || exit
+/usr/sbin/semodule -i trivalent.pp -v 
 
-/sbin/restorecon -F -R -v /usr/lib/chromium-browser/
 /sbin/restorecon -F -R -v /usr/lib/trivalent/
