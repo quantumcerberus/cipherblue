449cde4e8701e465957b6786aa014aef60a4c4ba
quantumcerberus <182113870+quantumcerberus@users.noreply.github.com>
2025-02-12 11:48:34 +0530

    Update hardenuserns.sh

--- files/scripts/hardenuserns.sh
+++ files/scripts/hardenuserns.sh
@@ -20,3 +20,5 @@ semodule -i ./selinux/user_namespace/grant_userns.cil
 semodule -i ./selinux/user_namespace/harden_userns.cil
 semodule -i ./selinux/user_namespace/harden_container_userns.cil
 semodule -i ./selinux/flatpakfull/grant_systemd_flatpak_exec.cil
+
+dnf5 remove selinux-policy-devel -y
