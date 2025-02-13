45f8f1153ceae3ab86d1c7c8cb0b6636eb20f0df
quantumcerberus <182113870+quantumcerberus@users.noreply.github.com>
2025-02-13 13:40:14 +0530

    Update cipherblue-cleanup.sh

--- files/scripts/cipherblue-cleanup.sh
+++ files/scripts/cipherblue-cleanup.sh
@@ -7,4 +7,3 @@ rm -f /etc/yum.repos.d/!(fedora.repo|fedora-updates.repo)
 rm -f /etc/fwupd/remotes.d/lvfs-testing.conf
 rm -f /usr/libexec/gnome-software-dkms-helper
 rm -f /etc/xdg/autostart/org.gnome.Software.desktop
-rm -f /etc/dnf/protected.d/sudo.conf
